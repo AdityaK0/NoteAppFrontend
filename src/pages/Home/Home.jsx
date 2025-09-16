ec183d15ff21d59fddcac0feec03e6ad60cbc013
AdityaTeam8 <adiya.chaudhary@team8network.com>
2025-09-16 12:25:56 +0530

    Stable Add icon

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -209,7 +209,7 @@ function Home() {
 
 
 
-      
+{/*       
      <button className='w-16 h-16 flex items-center  justify-center rounded-2xl bg-blue-500 hover:bg-blue-600 absolute right-10 bottom-10 ' 
       onClick={()=>{
         setOpenAddEditModal({isShown:true,type:"add",data:null})
@@ -217,7 +217,19 @@ function Home() {
 
 
         <MdAdd className='text-[32px] text-white'/>
-      </button>
+      </button> */}
+
+      <button
+  className="w-16 h-16 flex items-center justify-center 
+             rounded-2xl bg-blue-500 hover:bg-blue-600 
+             fixed right-10 bottom-10 z-50 shadow-lg"
+  onClick={() => {
+    setOpenAddEditModal({ isShown: true, type: "add", data: null });
+  }}
+>
+  <MdAdd className="text-[32px] text-white" />
+</button>
+
 
       <Modal
                isOpen = {openAddEditModal.isShown}
