27539a4aa6e02b584256169300174584fb1f1a2b
AdityaK0 <aditya123905@gmail.com>
2025-12-19 22:31:23 +0530

    done done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,1 @@
-export const BASE_URL = "http://127.0.0.1:8001/"   
-
-// export const BASE_URL = "https://noteapphub.duckdns.org/"   
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://noteapphub.duckdns.org/";
